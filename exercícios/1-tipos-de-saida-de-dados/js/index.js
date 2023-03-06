e86c364ee6ea55137d98ef063b391ffae10043b9
Keison Roney <110565730+keisonroney@users.noreply.github.com>
2023-03-06 17:42:38 -0300

    arquivos javascript adicionados

---
+++ exercícios/1-tipos-de-saida-de-dados/js/index.js
@@ -0,0 +1,3 @@
+document.getElementById("texto").innerHTML="Meu Primeiro Texto <b>JS</b>!"; //faz interação com html
+//alert('alerta');
+console.log("Isso é um console.log"); //exibe dados no console.log
