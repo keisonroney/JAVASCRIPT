65907d081d7f91f9f64be4f74f4709efa07d9793
Keison Roney <110565730+keisonroney@users.noreply.github.com>
2023-03-06 17:49:45 -0300

    Delete EXERCÍCIOS/1-tipos-de-saida-de-dados directory

--- EXERCÍCIOS/1-tipos-de-saida-de-dados/js/index.js
+++
@@ -1,3 +0,0 @@
-document.getElementById("texto").innerHTML="Meu Primeiro Texto <b>JS</b>!"; //faz interação com html
-//alert('alerta');
-console.log("Isso é um console.log"); //exibe dados no console.log
